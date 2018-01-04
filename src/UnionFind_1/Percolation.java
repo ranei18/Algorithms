316a999bb9ebecf3a7f6675fc5a4eab1b51e7b10
Remy Anderson <remy.c.anderson@gmail.com>
2018-01-03 18:19:16 -0600

    consolidated logic

--- src/UnionFind_1/Percolation.java
+++ src/UnionFind_1/Percolation.java
@@ -1,5 +1,6 @@
-package UnionFind_1;
-//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//package UnionFind_1;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private final boolean[][] grid;
@@ -7,61 +8,84 @@ public class Percolation {
     private final int topNode = 0;
     private final int bottomNode;
     private int openSites = 0;
-    private final WeightedQuickUnionUF full;       
+    private final WeightedQuickUnionUF full;
+    private final WeightedQuickUnionUF open;
     
-    public Percolation(int n){
+    public Percolation(int n) {
         grid = new boolean[n][n];
         gridSize = n;
         bottomNode = n * n + 1;
-        full = new WeightedQuickUnionUF(n * n + 2);
+        open = new WeightedQuickUnionUF(n * n + 2);
+        full = new WeightedQuickUnionUF(n * n + 1);
     }
     
-    public int xyTo1D(int row, int col){
-        return (row - 1)*gridSize + col;
+    private int xyTo1D(int row, int col) {
+        return (row - 1) * gridSize + col;
     }
     
-    public boolean isOpen(int row, int col){
-        return grid[row -1][col - 1];
+    private boolean invalidInput(int row, int col) {
+        if ( row > 0 && row <= gridSize && col > 0 && col <= gridSize) {
+            return false;
+        } else {
+			return true;
+		}
     }
     
-    public int numberOfOpenSites(){
+    public boolean isOpen(int row, int col) {
+        if (invalidInput(row , col)) {
+			throw new IllegalArgumentException();
+		} 
+        return grid[row -1][col - 1];    
+
+    }
+    
+    public int numberOfOpenSites() {
         return openSites;
     }
     
-    public boolean percolates(){
-        return full.connected(topNode,bottomNode);
+    public boolean percolates() {
+        return open.connected(topNode,bottomNode);
     }
     
-    public boolean isFull(int row, int col){
-        return full.connected(topNode,xyTo1D(row, col));
+    public boolean isFull(int row, int col) {
+        if (invalidInput(row , col)) {
+			throw new IllegalArgumentException();
+		} 
+        return full.connected(topNode,xyTo1D(row, col)); 
     }
     
-    public void open(int row, int col){
-        if(!isOpen(row,col)){
+    public void open(int row, int col) {
+        if(!isOpen(row,col)) {
             grid[row - 1][col - 1] = true;
-            openSites += 1;
-        }
-        
-        matchIfOpen(xyTo1D(row , col), row - 1, col);
-        matchIfOpen(xyTo1D(row , col), row + 1, col);
-        matchIfOpen(xyTo1D(row , col), row, col - 1);
-        matchIfOpen(xyTo1D(row , col), row, col + 1);    
-        if(row == 1){
-            matchIfOpen(topNode, row, col);
-        }
-        if(row == gridSize && isFull(row, col)){
-            matchIfOpen(bottomNode, row, col);
-        }
-        
-    }
-    
-    public void matchIfOpen(int newlyOpened, int row, int col){
-        try{
-            if (isOpen(row,col)){
-                full.union(newlyOpened,xyTo1D(row, col));
+            openSites++;
+            int openedNode = xyTo1D(row , col);
+			
+            //join newly opened site to surrounding sites
+            if (row > 1 && isOpen(row - 1, col)) {
+                open.union(openedNode, openedNode - gridSize);
+                full.union(openedNode, openedNode - gridSize); 
             }
-        } catch (IndexOutOfBoundsException e){
-            //do nothing
-        }
+            if (row < gridSize && isOpen(row + 1, col)) {
+                open.union(openedNode, openedNode + gridSize);
+                full.union(openedNode, openedNode + gridSize);
+            }
+            if (col > 1 && isOpen(row , col - 1)) {
+                open.union(openedNode, openedNode - 1); 
+                full.union(openedNode, openedNode - 1);
+            }
+            if (col < gridSize && isOpen(row , col + 1)) {
+                open.union(openedNode, openedNode + 1);
+                full.union(openedNode, openedNode + 1);
+            }           
+            
+            //join top and bottom row to virtual node
+            if(row == 1){
+                open.union(topNode, openedNode);
+                full.union(topNode, openedNode);
+            }
+            if(row == gridSize) {
+                open.union(bottomNode, openedNode);
+            }			
+        } 
     }
 }
