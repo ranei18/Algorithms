b6bb168f2de13424e47132b0baefd5ea80bbe906
Remy Anderson <remy.c.anderson@gmail.com>
2018-01-05 16:02:44 -0600

    Added generic Stack

---
+++ src/Week2/IterableStack.java
@@ -0,0 +1,50 @@
+package Week2;
+
+import java.util.Iterator;
+
+public class IterableStack<Item> implements Iterable<Item> {
+    //generic implementation of LinkedStackOfStrings
+    private Node first = null;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        return item;
+    }
+
+    //Create iterator for stack
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    //Create iterator methods
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
